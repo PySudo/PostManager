a307a61186e57f177d885eb168c04543ac7612fd
PySudo <omid377773@gmail.com>
2025-04-01 21:02:37 +0330

    add channelString

--- core/functions.py
+++ core/functions.py
@@ -13,6 +13,9 @@ def checkJoin(user, channels):
             ch.append(channel)
     return ch
 
+def channelString(channels):
+    return '\n'.join(i if i[0] == '@' else '@'+i for i in channels)
+
 def getJoinText(channel):
     text = "❗️ لطفا اول توی کانال جوین شو :\n{}"
     return text.format(channel)
